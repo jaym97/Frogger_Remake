692a4e4d25af5d0ac67e425c5e3c599341d7fef4
jaym97 <34835538+jaym97@users.noreply.github.com>
2018-09-04 15:50:51 +0100

    feat: Add code to make site a PWA

---
+++ js/sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(['index.html','/css/style.css','/js/app.js','/js/engine.js','/js/resources.js']); toolbox.router.get('/images/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
